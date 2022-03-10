6701695dcc3680b881a135d061b30e2e0054d30d
a.wojnicki <wojnicki.adam1@gmail.com>
2022-03-10 16:25:15 +0100

    mibile nav + initial hero styling

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "./style.css";
+
+// MOBILE MENU FUCNTIONALITY:
+const mobileBtn: HTMLButtonElement = document.querySelector(".mobile-btn")!;
+const mobileNav: HTMLMenuElement = document.querySelector(".mobile-nav")!;
+
+mobileBtn.addEventListener("click", () => {
+  mobileNav.classList.toggle("hidden");
+});
