d1b2f991f27193aebd8dcb2455d2f8b21f54e089
a.wojnicki <wojnicki.adam1@gmail.com>
2022-03-24 16:14:57 +0100

    smooth css scrolling

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// STYLE IMPORTS
 import "./style.css";
 
 // MOBILE MENU FUCNTIONALITY:
@@ -6,4 +7,25 @@ const mobileNav: HTMLMenuElement = document.querySelector(".mobile-nav")!;
 
 mobileBtn.addEventListener("click", () => {
   mobileNav.classList.toggle("hidden");
+});
+
+// ACTIVE NAV LINKS
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const activeItem = document.querySelector(".nav-item.active")!;
+    activeItem.classList.remove("active");
+    item.classList.add("active");
+  });
+});
+
+// CLOSE MOBILE MENU AFTER CLICKING A LINK
+const mobileLinks: NodeListOf<HTMLAnchorElement> =
+  document.querySelectorAll(".mobile-nav a");
+
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileNav.classList.add("hidden");
+  });
 });
